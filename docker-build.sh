b5b3faf1ef89f06b431d59e6ae3181002b476a81
Enrique <enriqueroman@live.com>
2025-04-30 11:42:25 +0200

    docker-build.sh

--- docker-build.sh
+++ docker-build.sh
@@ -10,7 +10,7 @@ export BENCHBASE_PROFILES="$BENCHBASE_PROFILE"
 # Specify a different version of the profile to use (suffix in this directory).
 export PROFILE_VERSION='latest'
 # Whether or not to rebuild the package/image.
-export CLEAN_BUILD="false"
+export CLEAN_BUILD="true"
 # When rebuilding, whether or not to run the unit tests.
 export SKIP_TESTS="true"
 
